e00eb8cee538ca4fd74ecfedddc31ec0cee51722
aguindehi <amir@datacore.ch>
2022-06-28 13:19:56 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const request = require('request');
 
 const app = express();
+const appInsights = require("applicationinsights");
+appInsights.setup("0db2fba7-dbed-44f4-b6cd-9833d536311b");
+appInsights.start();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
